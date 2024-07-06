671ec817099083cadea1250c0fcc4f266599a64c
kotudemo <130310308+kotudemo@users.noreply.github.com>
2024-07-06 21:48:21 +0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -279,11 +279,11 @@
                 isNormalUser = true;
                 initialPassword = "password";
                 packages = with pkgs; [
-		                  vesktop
-			                vscode
-                      spotify
-		                  spicetify-cli
-		                  _64gram
+		    vesktop
+		    vscode
+                    spotify
+		    spicetify-cli
+		    _64gram
                 ];
             };
         };
